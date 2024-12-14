66bcc51e190e98f06b50952c8fead267675edd04
HaiderMalikk <haidermalik662@gmail.com>
2024-12-14 00:13:42 -0500

    streamlit app v2

--- app/functions.py
+++ app/functions.py
@@ -1,4 +1,11 @@
+# Backend llm calculations (all frontend code is in streamlit_app.py)
+""" 
+This python file is equivalent to the notebook data_extraction.ipynb except that it is imported into streamlit_app.py
+also i added a rag chain equivalent to the one in the notebook, i also used some os and tempfile modules to work with the files 
+also i added uuid to unique ids for the vector store text embeddings to avoid duplicate embeddings from the overlapping chunks
+"""
 
+# all imports (see notebook for more info)
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
@@ -7,178 +14,119 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 
-import os
-import tempfile
-import uuid
-import pandas as pd
-import re
+import os # to delete the temporary file for temporary file
+import tempfile # to create a temporary file for PyPDFLoader
+import uuid # to create a unique id for the temporary file for PyPDFLoader
+import pandas as pd # to convert the pdf to a dataframe and then to a table
+import re # to clean the text  
 
+# used to clean up the file name  (as we need it later)
 def clean_filename(filename):
-    """
-    Remove "(number)" pattern from a filename 
-    (because this could cause error when used as collection name when creating Chroma database).
-
-    Parameters:
-        filename (str): The filename to clean
-
-    Returns:
-        str: The cleaned filename
-    """
-    # Regular expression to find "(number)" pattern
+    # Regular expression to find "(number)" pattern in filename
     new_filename = re.sub(r'\s\(\d+\)', '', filename)
-    return new_filename
+    return new_filename # return the cleaned filename
 
+# loading the text from the pdf file
 def get_pdf_text(uploaded_file): 
-    """
-    Load a PDF document from an uploaded file and return it as a list of documents
-
-    Parameters:
-        uploaded_file (file-like object): The uploaded PDF file to load
-
-    Returns:
-        list: A list of documents created from the uploaded PDF file
-    """
-    try:
+    try: # to handle errors 
         # Read file content
         input_file = uploaded_file.read()
 
-        # Create a temporary file (PyPDFLoader requires a file path to read the PDF,
-        # it can't work directly with file-like objects or byte streams that we get from Streamlit's uploaded_file)
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        temp_file.write(input_file)
-        temp_file.close()
+        # Create a temporary file (PyPDFLoader requires a file path to read the PDF hence we need to create a temporary file)
+        # it can't work directly with file-like objects or byte streams so we must pass in the actual file
+        # in the Streamlit app the uploaded_file is converted to a byte stream to be displayed not here
+        temp_file = tempfile.NamedTemporaryFile(delete=False) # delete=False to keep the file after it is closed 
+        temp_file.write(input_file) # write the content of the uploaded file to the temporary file
+        temp_file.close() # close the temporary file
 
-        # load PDF document
-        loader = PyPDFLoader(temp_file.name)
-        documents = loader.load()
+        # load PDF document using PyPDFLoader
+        loader = PyPDFLoader(temp_file.name) # pass the path to the temporary file
+        documents = loader.load() # load the pdf document
 
-        return documents
+        return documents # return the loaded pdf document
     
-    finally:
-        # Ensure the temporary file is deleted when we're done with it
-        os.unlink(temp_file.name)
+    finally: # to delete the temporary file once we are done
+        os.unlink(temp_file.name) # delete the temporary file
 
-
-def split_document(documents, chunk_size, chunk_overlap):    
-    """
-    Function to split generic text into smaller chunks.
-    chunk_size: The desired maximum size of each chunk (default: 400)
-    chunk_overlap: The number of characters to overlap between consecutive chunks (default: 20).
-
-    Returns:
-        list: A list of smaller text chunks created from the generic text
-    """
+# splitting the text into chunks
+def split_document(documents, chunk_size, chunk_overlap): 
+    # split the text into chunks
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
-                                          chunk_overlap=chunk_overlap,
-                                          length_function=len,
-                                          separators=["\n\n", "\n", " "])
+                                          chunk_overlap=chunk_overlap, # overlap between chunks
+                                          length_function=len, # function to determine the length of a chunk
+                                          separators=["\n\n", "\n", " "]) # separators to split on here we split on new linee pages and spaces
     
+    # return the chunks
     return text_splitter.split_documents(documents)
 
-
+# get the embedding function from the OpenAI API
 def get_embedding_function(api_key):
-    """
-    Return an OpenAIEmbeddings object, which is used to create vector embeddings from text.
-    The embeddings model used is "text-embedding-ada-002" and the OpenAI API key is provided
-    as an argument to the function.
-
-    Parameters:
-        api_key (str): The OpenAI API key to use when calling the OpenAI Embeddings API.
-
-    Returns:
-        OpenAIEmbeddings: An OpenAIEmbeddings object, which can be used to create vector embeddings from text.
-    """
+    # create embedding function
     embeddings = OpenAIEmbeddings(
-        model="text-embedding-ada-002", openai_api_key=api_key
+        model="text-embedding-ada-002", openai_api_key=api_key # using the text-embedding-ada-002 model 
     )
+    # return the embedding function
     return embeddings
 
-
+# creating the vector store database for the chunks to be stored and compared
 def create_vectorstore(chunks, embedding_function, file_name, vector_store_path="db"):
-
-    """
-    Create a vector store from a list of text chunks.
-
-    :param chunks: A list of generic text chunks
-    :param embedding_function: A function that takes a string and returns a vector
-    :param file_name: The name of the file to associate with the vector store
-    :param vector_store_path: The directory to store the vector store
-
-    :return: A Chroma vector store object
-    """
-
     # Create a list of unique ids for each document based on the content
     ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, doc.page_content)) for doc in chunks]
     
     # Ensure that only unique docs with unique ids are kept
-    unique_ids = set()
-    unique_chunks = []
+    # this avoids duplicate documents in the vector store for overlapping chunks
+    unique_ids = set() # create a set to store unique ids a set can't have duplicates
+    unique_chunks = [] # create a list to store unique chunks, we check the id before we add it so we dont need a set here
     
-    unique_chunks = [] 
-    for chunk, id in zip(chunks, ids):     
-        if id not in unique_ids:       
-            unique_ids.add(id)
-            unique_chunks.append(chunk)        
+    for chunk, id in zip(chunks, ids): # iterate over the chunks and their ids, we use zip to map each chunk to its id
+        if id not in unique_ids: # if the id is not in the set
+            unique_ids.add(id) # add the id to the set
+            unique_chunks.append(chunk) # add the chunk to the list
 
-    # Create a new Chroma database from the documents
-    vectorstore = Chroma.from_documents(documents=unique_chunks, 
-                                        collection_name=clean_filename(file_name),
-                                        embedding=embedding_function, 
-                                        ids=list(unique_ids), 
-                                        persist_directory = vector_store_path)
+    # Create a new Chroma database from the documents chunks and use the embedding function
+    vectorstore = Chroma.from_documents(documents=unique_chunks, # pass in the unique chunks
+                                        collection_name=clean_filename(file_name), # clean the file name to use it as the collection name
+                                        embedding=embedding_function, # pass in the embedding function
+                                        ids=list(unique_ids), # pass in the unique ids
+                                        persist_directory = vector_store_path) # pass in the vector store path
 
     # The database should save automatically after we create it
     # but we can also force it to save using the persist() method
     vectorstore.persist()
     
+    # return the vector store database
     return vectorstore
 
-
+# creating the vector store from our pdf text 
+# this is where we call the split_document to split the pdf into chunks and the pass that into the vector store along with the embedding function
+# all our previous helper functions are called here, but since we need a new db for each pdf we call this function in streamlit app file
 def create_vectorstore_from_texts(documents, api_key, file_name):
-    
-    # Step 2 split the documents  
-    """
-    Create a vector store from a list of texts.
-
-    :param documents: A list of generic text documents
-    :param api_key: The OpenAI API key used to create the vector store
-    :param file_name: The name of the file to associate with the vector store
-
-    :return: A Chroma vector store object
-    """
+    # split the text into chunks
     docs = split_document(documents, chunk_size=1000, chunk_overlap=200)
     
-    # Step 3 define embedding function
+    # define embedding function
     embedding_function = get_embedding_function(api_key)
 
-    # Step 4 create a vector store  
+    # create a vector store now that we have the text and embedding function to pass into the vector store
     vectorstore = create_vectorstore(docs, embedding_function, file_name)
     
+    # return the vector store now filled with the pdf text
     return vectorstore
 
-
+# main function to load the vector store
+# this function is called after we create the vector store in the streamlit app
 def load_vectorstore(file_name, api_key, vectorstore_path="db"):
-
-    """
-    Load a previously saved Chroma vector store from disk.
-
-    :param file_name: The name of the file to load (without the path)
-    :param api_key: The OpenAI API key used to create the vector store
-    :param vectorstore_path: The path to the directory where the vector store was saved (default: "db")
-    
-    :return: A Chroma vector store object
-    """
     embedding_function = get_embedding_function(api_key)
     return Chroma(persist_directory=vectorstore_path, 
                   embedding_function=embedding_function, 
                   collection_name=clean_filename(file_name))
 
-# Prompt template
+# Prompt template (little different from the original in notebook)
 PROMPT_TEMPLATE = """
-You are an assistant for question-answering tasks.
+You are an assistant for answering questions about a research paper.
 Use the following pieces of retrieved context to answer
 the question. If you don't know the answer, say that you
-don't know. DON'T MAKE UP ANYTHING.
+don't know. DON'T MAKE UP ANYTHING!
 
 {context}
 
@@ -187,13 +135,14 @@ don't know. DON'T MAKE UP ANYTHING.
 Answer the question based on the above context: {question}
 """
 
+# class to get sources and resoning for our answer
 class AnswerWithSources(BaseModel):
     """An answer to the question, with sources and reasoning."""
     answer: str = Field(description="Answer to question")
     sources: str = Field(description="Full direct text chunk from the context used to answer the question")
     reasoning: str = Field(description="Explain the reasoning of the answer based on the sources")
     
-
+# class to pass in the sources and resoning for our answer to the question about title, summary, publication date and author
 class ExtractedInfoWithSources(BaseModel):
     """Extracted information about the research article"""
     paper_title: AnswerWithSources
@@ -201,55 +150,48 @@ class ExtractedInfoWithSources(BaseModel):
     publication_year: AnswerWithSources
     paper_authors: AnswerWithSources
 
+# function to format the doc passed in into a single string joined by two newlines
 def format_docs(docs):
-    """
-    Format a list of Document objects into a single string.
-
-    :param docs: A list of Document objects
-
-    :return: A string containing the text of all the documents joined by two newlines
-    """
     return "\n\n".join(doc.page_content for doc in docs)
 
+# in this function we create the prompt template and the rag chain and pass in out question to the llm
 # retriever | format_docs passes the question through the retriever, generating Document objects, and then to format_docs to generate strings;
 # RunnablePassthrough() passes through the input question unchanged.
-def query_document(vectorstore, query, api_key):
-
-    """
-    Query a vector store with a question and return a structured response.
-
-    :param vectorstore: A Chroma vector store object
-    :param query: The question to ask the vector store
-    :param api_key: The OpenAI API key to use when calling the OpenAI Embeddings API
-
-    :return: A pandas DataFrame with three rows: 'answer', 'source', and 'reasoning'
-    """
+def query_document(vectorstore, query, api_key): # pass in the vector store and the question to be asked along with the api key
+    # define llm
     llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
-
+    # define retriever (gets text chunks from vector store based on similarity)
     retriever=vectorstore.as_retriever(search_type="similarity")
-
+    # define prompt template with context and question
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-
+    # define rag chain, this calls the retriever to get context and then passes the context and question into the prompt template
+    # then finally passes the prompt template into the llm with structured output, struct defined as ExtractedInfoWithSources this is strict
     rag_chain = (
             {"context": retriever | format_docs, "question": RunnablePassthrough()}
             | prompt_template
             | llm.with_structured_output(ExtractedInfoWithSources, strict=True)
         )
-
+    # call the rag chain to get the structured response, this is where we call the llm
     structured_response = rag_chain.invoke(query)
+    
+    # with this new structured response we can create a table to organize our data
+    # we use pandas to create a dataframe and pass in out the structured response as a dictionary
     df = pd.DataFrame([structured_response.dict()])
 
-    # Transforming into a table with two rows: 'answer' and 'source'
+    # Transforming into a table with 3 rows onw for each field in the answer
     answer_row = []
     source_row = []
     reasoning_row = []
 
+    # for each column in the dataframe i.e for each question we ask we have 3 rows for the answer, sources and reasoning for that question
     for col in df.columns:
-        answer_row.append(df[col][0]['answer'])
-        source_row.append(df[col][0]['sources'])
-        reasoning_row.append(df[col][0]['reasoning'])
+        answer_row.append(df[col][0]['answer']) # we extract the answer from the df and add it to the answer row
+        source_row.append(df[col][0]['sources']) # do the same for the sources
+        reasoning_row.append(df[col][0]['reasoning']) # do the same for the reasoning
 
-    # Create new dataframe with two rows: 'answer' and 'source'
+    # Create new dataframe with 3 rows we will add the created rows to this dataframe table
+    # we pass in our rows as we have created them above, pass in our columns as they are in the dataframe and a list of indexes to act as headers i.e column names
     structured_response_df = pd.DataFrame([answer_row, source_row, reasoning_row], columns=df.columns, index=['answer', 'source', 'reasoning'])
   
+    # return the structured response dataframe trasposed i.e rows become columns, for readability
     return structured_response_df.T
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,89 +1,72 @@
-import streamlit as st  
-from functions import *
-import base64
+# Streamlit app UI (all backend code is in functions.py)
+""" 
+This is a Streamlit app that allows users to upload a PDF document and generate a structured response.
+the outline for this app is in this file, it uses the calculations from the functions.py file and displays the results in a Streamlit app
+"""
+
+import streamlit as st  # streamlit
+from functions import * # import all functions from functions.py
+import base64 # for encoding and decoding
 
 # Initialize the API key in session state if it doesn't exist
 if 'api_key' not in st.session_state:
     st.session_state.api_key = ''
 
-def display_pdf(uploaded_file):
-
-    """
-    Display a PDF file that has been uploaded to Streamlit.
-
-    The PDF will be displayed in an iframe, with the width and height set to 700x1000 pixels.
-
-    Parameters
-    ----------
-    uploaded_file : UploadedFile
-        The uploaded PDF file to display.
-
-    Returns
-    -------
-    None
-    """
+def display_pdf(uploaded_file): # after file is uploaded display it
     # Read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    
     # Convert to Base64
     base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
-    
-    # Embed PDF in HTML
+    # Embed PDF in HTML and display on screen
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    
-    # Display file
+    # Display file in Streamlit
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-
-def load_streamlit_page():
-
-    """
-    Load the streamlit page with two columns. The left column contains a text input box for the user to input their OpenAI API key, and a file uploader for the user to upload a PDF document. The right column contains a header and text that greet the user and explain the purpose of the tool.
-
-    Returns:
-        col1: The left column Streamlit object.
-        col2: The right column Streamlit object.
-        uploaded_file: The uploaded PDF file.
-    """
+# Main app page
+def load_streamlit_page(): 
     st.set_page_config(layout="wide", page_title="LLM Tool")
-
     # Design page layout with 2 columns: File uploader on the left, and other interactions on the right.
     col1, col2 = st.columns([0.5, 0.5], gap="large")
-
-    with col1:
+    with col1: # left column
+        # stuff for left column on page
         st.header("Input your OpenAI API key")
         st.text_input('OpenAI API key', type='password', key='api_key',
                     label_visibility="collapsed", disabled=False)
         st.header("Upload file")
         uploaded_file = st.file_uploader("Please upload your PDF document:", type= "pdf")
 
-    return col1, col2, uploaded_file
+    return col1, col2, uploaded_file # export col1 and col2 to be used in creating the app
 
 
-# Make a streamlit page
-col1, col2, uploaded_file = load_streamlit_page()
+# Make a streamlit page using the load function above
+col1, col2, uploaded_file = load_streamlit_page()  # call the load function and load the vvalues into the variables
 
-# Process the input
+# if we have a file Process the input from it 
 if uploaded_file is not None:
-    with col2:
-        display_pdf(uploaded_file)
+    with col2: # right column
+        display_pdf(uploaded_file) # display the uploaded file
         
     # Load in the documents
-    documents = get_pdf_text(uploaded_file)
+    documents = get_pdf_text(uploaded_file) # from our functions.py we get the pdf text using pypdfloader
+    # creating a vector store to store the text emmbeddings
     st.session_state.vector_store = create_vectorstore_from_texts(documents, 
                                                                   api_key=st.session_state.api_key,
                                                                   file_name=uploaded_file.name)
-    st.write("Input Processed")
+    st.write("Input Processed") # finish statement
 
-# Generate answer
-with col1:
-    if st.button("Generate table"):
-        with st.spinner("Generating answer"):
-            # Load vectorstore:
-
+# Generate answer using the vector store
+with col1: # left column
+    if st.button("Generate table"): # if the button is clicked
+        with st.spinner("Generating answer"): # display a spinner that will wait until the answer is generated
+            # answer if now generated and ready
+            
+            # Load into the variable answer vectorstore, query and api key into the query document function which dose all the rag chain processing 
             answer = query_document(vectorstore = st.session_state.vector_store, 
                                     query = "Give me the title, summary, publication date, and authors of the research paper.",
                                     api_key = st.session_state.api_key)
                             
-            placeholder = st.empty()
-            placeholder = st.write(answer)
+            # display the answer
+            placeholder = st.empty() # create an empty placeholder to display the answer
+            placeholder = st.write(answer) # display the answer in the placeholder
+            
+# * done
